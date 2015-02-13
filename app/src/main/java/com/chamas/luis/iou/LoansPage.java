285c94e293dc1fab7444ae6c6d4b692c1eec5c98
Luis <albertorio.10@hotmail.com>
2015-02-12 23:37:00 -0300

    Started working on app

---
+++ app/src/main/java/com/chamas/luis/iou/LoansPage.java
@@ -0,0 +1,63 @@
+package com.chamas.luis.iou;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.chamas.luis.iou.R;
+
+public class LoansPage extends Activity {
+    private Button debt;
+    private Button loan;
+    private ListView owe;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loans_page);
+
+        String[] Money = {"Luis", "Albertorio"};
+
+        debt = (Button)findViewById(R.id.LoansPageDebtsButton);
+        loan = (Button)findViewById(R.id.LoansPageLoansButton);
+        owe = (ListView)findViewById(R.id.LoansListView);
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,Money);
+        owe.setAdapter(theAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.loans_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.exit) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void DebtsPage(View view) {
+        Intent getDebtsPage = new Intent(this, MyActivity.class);
+        startActivity(getDebtsPage);
+    }
+
+    public void LoansPage(View view) {
+        Intent getLoansPage = new Intent(this, LoansPage.class);
+        startActivity(getLoansPage);
+    }
+}
